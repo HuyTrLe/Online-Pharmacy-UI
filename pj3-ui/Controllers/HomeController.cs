ac6170d3527a5144f18869adb571b6bc001be6c7
NamNguyen2604 <nguyendinhnam5351@gmail.com>
2023-10-03 22:03:08 +0700

    update

--- pj3-ui/Controllers/HomeController.cs
+++ pj3-ui/Controllers/HomeController.cs
@@ -39,8 +39,17 @@ namespace pj3_ui.Controllers
 
             return View();
         }
+		public ActionResult Recruitment()
+		{
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+			return View();
+		}
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
